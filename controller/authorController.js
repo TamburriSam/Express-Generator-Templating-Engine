19b72eb8c69e6f14b3e24767930b3657be4c8b90
tamburrisam <tamburrisam@gmail.com>
2021-06-25 15:09:14 -0400

    added update author view/controller

--- controller/authorController.js
+++ controller/authorController.js
@@ -184,11 +184,91 @@ exports.author_delete_post = function (req, res, next) {
 };
 
 // Display Author update form on GET.
-exports.author_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author update GET");
+exports.author_update_get = function (req, res, next) {
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        let err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+      }
+      //success
+      res.render("author_form", {
+        title: "Update Author",
+        author: results.author,
+      });
+    }
+  );
 };
 
 // Handle Author update on POST.
 exports.author_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author update POST");
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+    body("family_name")
+      .trim()
+      .isLength({ min: 1 })
+      .escape()
+      .withMessage("Family name must be specified.")
+      .isAlphanumeric()
+      .withMessage("Family name has non-alphanumeric characters."),
+    body("date_of_birth", "Invalid date of birth")
+      .optional({ checkFalsy: true })
+      .isISO8601()
+      .toDate(),
+    body("date_of_death", "Invalid date of death")
+      .optional({ checkFalsy: true })
+      .isISO8601()
+      .toDate(),
+    (req, res, next) => {
+      const errors = validationResult(req);
+
+      //create new author object with escaped and trimmed data
+      let author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+
+      //there are errors- render form again w sanitized errors and values
+      if (!errors.isEmpty()) {
+        async.parallel({
+          author: function (callback) {
+            Author.findById(req.params.id).exec(callback);
+          },
+        });
+
+        res.render("author_form", {
+          title: "Update Author",
+          author: results.author,
+        });
+      } else {
+        Author.findByIdAndUpdate(
+          req.params.id,
+          author,
+          {},
+          function (err, theauthor) {
+            if (err) {
+              return next(err);
+            }
+            res.redirect(theauthor.url);
+          }
+        );
+      }
+    };
 };
